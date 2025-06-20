9b5ccaae4c3e055d35b2aba128467a43de23fda6
herman-xphp <hermanxphp@gmail.com>
2025-06-20 11:03:59 +0800

    setup project

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+const store = null;
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
