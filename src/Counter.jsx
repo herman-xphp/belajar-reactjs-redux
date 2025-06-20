35715a720c6ac922a1a669e90c92e5c09a069786
herman-xphp <hermanxphp@gmail.com>
2025-06-20 11:50:49 +0800

    Action

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./CounterSlice";
 
 export default function Counter() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
 
   return (
     <div>
       <h1>Counter : {counter}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={() => dispatch(increment(2))}>Increment + 2</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={() => dispatch(decrement(2))}>Decrement - 2</button>
     </div>
   );
 }
--- src/counterSlice.js
+++ src/counterSlice.js
@@ -3,5 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: 0,
-  reducers: {},
+  reducers: {
+    increment: (state, action) => {
+      if (action.payload) {
+        return state + action.payload;
+      } else {
+        return state + 1;
+      }
+    },
+    decrement: (state, action) => {
+      if (action.payload) {
+        return state - action.payload;
+      } else {
+        return state - 1;
+      }
+    },
+  },
 });
+
+export const { increment, decrement } = counterSlice.actions;
