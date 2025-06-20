7c392cc48626abdedc3a20132a83492c6740fe9d
herman-xphp <hermanxphp@gmail.com>
2025-06-20 11:25:23 +0800

    state

---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+
+export default function Counter() {
+  const counter = useSelector((state) => state.counter);
+
+  return (
+    <div>
+      <h1>Counter : {counter}</h1>
+    </div>
+  );
+}
